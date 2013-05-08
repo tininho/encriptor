e0c766290e9bcd2039d96afd454503b95f6f04be
Florentino Romero <fromeroharo@gmail.com>
2013-05-08 22:04:55 +0200

    clase de test de caracterizacion

---
+++ src/com/iexpertos/encriptor/EncriptorRunnerCharacterizationTest.java
@@ -0,0 +1,14 @@
+package com.iexpertos.encriptor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncriptorRunnerCharacterizationTest {
+
+	@Test
+	public void shouldExecuteMainEncriptor() {
+		fail("Not yet implemented");
+	}
+
+}
