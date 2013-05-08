1948ffeb00cd77b3a3cb31110ba793d6e62bf356
Florentino Romero <fromeroharo@gmail.com>
2013-05-09 01:07:24 +0200

    test clase Printer

---
+++ src/com/iexpertos/encriptor/util/PrinterTest.java
@@ -0,0 +1,14 @@
+package com.iexpertos.encriptor.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrinterTest {
+
+	@Test
+	public void shouldGetArrayOfTwoWords() {
+		fail("Not yet implemented");
+	}
+
+}
