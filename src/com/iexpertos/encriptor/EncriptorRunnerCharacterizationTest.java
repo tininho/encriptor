814c1e34958f015163f039d9886e17c20156cfa2
Florentino Romero <fromeroharo@gmail.com>
2013-05-08 22:10:00 +0200

    inclusion de checker e implementacion

--- src/com/iexpertos/encriptor/EncriptorRunnerCharacterizationTest.java
+++ src/com/iexpertos/encriptor/EncriptorRunnerCharacterizationTest.java
@@ -1,14 +1,37 @@
 package com.iexpertos.encriptor;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 import org.junit.Test;
 
 public class EncriptorRunnerCharacterizationTest {
 
+	public class Checker extends OutputStream {
+
+		private Checksum checksum = new CRC32();
+
+		@Override
+		public void write(int b) throws IOException {
+			checksum.update(b);
+		}
+		
+		public long getChecksumValue(){
+			return this.checksum.getValue();
+		}
+	}
+	
 	@Test
 	public void shouldExecuteMainEncriptor() {
-		fail("Not yet implemented");
+		Checker checker = new Checker();
+		System.setOut(new PrintStream(checker));
+		Main.main(null);
+		assertNotNull(checker.getChecksumValue());
 	}
 
 }
