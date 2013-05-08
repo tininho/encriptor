578e2b782a7c9e5aa7815d88aacc75b0ff193da1
Florentino Romero <fromeroharo@gmail.com>
2013-05-09 01:12:59 +0200

    test clase Printer

--- src/com/iexpertos/encriptor/util/PrinterTest.java
+++ src/com/iexpertos/encriptor/util/PrinterTest.java
@@ -1,7 +1,15 @@
 package com.iexpertos.encriptor.util;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 
 public class PrinterTest {
 	
+	@Test
+	public void shouldPrintWordsBetweenLowerThanAndGreaterThanSymbols(){
+		fail("test not yet implemented");
+	}
 	
 }
