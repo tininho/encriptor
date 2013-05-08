c9e28addb48ed36d8d87904b36d87ebfd1bffda8
Florentino Romero <fromeroharo@gmail.com>
2013-05-08 22:13:04 +0200

    refactor de clase Checker

--- src/com/iexpertos/encriptor/EncriptorRunnerCharacterizationTest.java
+++ src/com/iexpertos/encriptor/EncriptorRunnerCharacterizationTest.java
@@ -2,29 +2,13 @@ package com.iexpertos.encriptor;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.zip.CRC32;
-import java.util.zip.Checksum;
 
 import org.junit.Test;
 
+import com.iexpertos.encriptor.util.Checker;
+
 public class EncriptorRunnerCharacterizationTest {
-
-	public class Checker extends OutputStream {
-
-		private Checksum checksum = new CRC32();
-
-		@Override
-		public void write(int b) throws IOException {
-			checksum.update(b);
-		}
-		
-		public long getChecksumValue(){
-			return this.checksum.getValue();
-		}
-	}
 	
 	@Test
 	public void shouldExecuteMainEncriptor() {
