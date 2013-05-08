7c647b89abcd472c817cf9d7dea88a351926db88
Florentino Romero <fromeroharo@gmail.com>
2013-05-09 01:26:58 +0200

    refactor

--- src/com/iexpertos/encriptor/util/PrinterTest.java
+++ src/com/iexpertos/encriptor/util/PrinterTest.java
@@ -1,29 +1,33 @@
 package com.iexpertos.encriptor.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.PrintStream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class PrinterTest {
 	
+	Checker checker;
+	Printer printer;
+	
+	@Before
+	public void initialization(){
+		checker = new Checker();
+		System.setOut(new PrintStream(checker));
+		printer = new Printer();
+	}
+	
 	@Test
 	public void shouldPrintWordsBetweenLowerThanAndGreaterThanSymbols(){
-		Checker checker = new Checker();
-		System.setOut(new PrintStream(checker));
-		Printer printer = new Printer();
 		printer.printWordsBetweenLowerThanAndGreaterThanSymbols("a b");
 		assertEquals(3439837024L, checker.getChecksumValue());
 	}
 	
 	@Test
 	public void shouldPrintSentence(){
-		Checker checker = new Checker();
-		System.setOut(new PrintStream(checker));
-		Printer printer = new Printer();
 		printer.printSentence("a b");
 		assertEquals(3697970883L, checker.getChecksumValue());
 	}
