2257933122197b62dc7eff4b56268352e1fbf202
A168891 <usuarioAdesis@adesis.com>
2013-05-17 11:06:23 +0200

    refactor: extraccion de clase

--- src/com/iexpertos/encriptor/util/SentenceValidatorTest.java
+++ src/com/iexpertos/encriptor/util/SentenceValidatorTest.java
@@ -6,19 +6,6 @@ import org.junit.Test;
 
 public class SentenceValidatorTest {
 	
-	class SentenceValidator{
-		
-		private static final String BLANK_SPACE = "";
-		
-		public void checkingIfWordContainsSingleBlankSpace(String string) throws InvalidParameterException {
-			
-			if(BLANK_SPACE.equals(string)){
-				throw new InvalidParameterException();
-			}
-		}
-		
-	}
-	
 	@Test(expected=InvalidParameterException.class)
 	public void shouldThrowInvalidParameterExceptionWhenEmptySentence() throws InvalidParameterException {
 		SentenceValidator sentenceValidator = new SentenceValidator();
