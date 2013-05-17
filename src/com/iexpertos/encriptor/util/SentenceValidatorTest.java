209aab5d0e7c64d740401d6681ee8f91595547fd
A168891 <usuarioAdesis@adesis.com>
2013-05-17 11:03:19 +0200

    test de validacion de frases con excepcion adecuada

--- src/com/iexpertos/encriptor/util/SentenceValidatorTest.java
+++ src/com/iexpertos/encriptor/util/SentenceValidatorTest.java
@@ -1,14 +1,16 @@
 package com.iexpertos.encriptor.util;
 
+import java.security.InvalidParameterException;
+
 import org.junit.Test;
 
 public class SentenceValidatorTest {
 	
 	class SentenceValidator{
 
-		public void checkingIfWordContainsSingleBlankSpace(String string) throws Exception {
+		public void checkingIfWordContainsSingleBlankSpace(String string) throws InvalidParameterException {
 			if("".equals(string)){
-				throw new Exception();
+				throw new InvalidParameterException();
 			}
 		}
 		
@@ -16,6 +18,12 @@ public class SentenceValidatorTest {
 	
 	@Test(expected=Exception.class)
 	public void shouldThrowExceptionWhenEmptySentence() throws Exception {
+		SentenceValidator sentenceValidator = new SentenceValidator();
+		sentenceValidator.checkingIfWordContainsSingleBlankSpace("");
+	}
+	
+	@Test(expected=InvalidParameterException.class)
+	public void shouldThrowInvalidParameterExceptionWhenEmptySentence() throws InvalidParameterException {
 		SentenceValidator sentenceValidator = new SentenceValidator();
 		sentenceValidator.checkingIfWordContainsSingleBlankSpace("");
 	}
