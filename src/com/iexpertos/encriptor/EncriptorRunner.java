e5a7ae18dde940772767decc7e26f5aa30fd4d74
A168891 <usuarioAdesis@adesis.com>
2013-05-17 10:55:49 +0200

    test de validacion de frases

--- src/com/iexpertos/encriptor/EncriptorRunner.java
+++ src/com/iexpertos/encriptor/EncriptorRunner.java
@@ -10,10 +10,10 @@ public class EncriptorRunner {
 	public static void main(String[] args) {
 		Encriptor encriptor = new Encriptor();
 		Printer printer = new Printer();
-		System.out.println("1:" + encriptor.cryptWord("hola"));
-		System.out.println("2:" + encriptor.cryptWordToNumbers("hola"));
-		System.out.println("3:" + encriptor.cryptWordReplacingChars("hola", "o"));
-		System.out.println("4:" + encriptor.cryptSentence("hola, que tal estas?"));
+		printer.printSentence("1:" + encriptor.cryptWord("hola"));
+		printer.printSentence("2:" + encriptor.cryptWordToNumbers("hola"));
+		printer.printSentence("3:" + encriptor.cryptWordReplacingChars("hola", "o"));
+		printer.printSentence("4:" + encriptor.cryptSentence("hola, que tal estas?"));
 		printer.printWordsBetweenLowerThanAndGreaterThanSymbols("hola, que tal estas?");
 
 	}
---
+++ src/com/iexpertos/encriptor/util/SentenceValidatorTest.java
@@ -0,0 +1,14 @@
+package com.iexpertos.encriptor.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SentenceValidatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
